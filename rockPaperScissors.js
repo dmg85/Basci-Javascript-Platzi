eb554588c48b4d74e1f5c4b11d82b3bb81af20bc
Diego Medina <damgz191@gmail.com>
2022-08-16 21:13:25 -0500

    "Commit with the cpu function working"

--- rockPaperScissors.js
+++ rockPaperScissors.js
@@ -1,7 +1,36 @@
 // Create a RPS game playing against the cpu, using variables, functions.
 
-let rock = 1;
-let paper = 2;
-let scissors = 3;
+rock = 1;
+paper = 2;
+scissors = 3;
+
+let randomCpu = Math.floor(Math.random() * 3) + 1;
+
+function cpu() {
+    if (randomCpu == 1) {
+        console.log('Cpu Choose rock');   
+    }  else if (randomCpu == 2) {
+        console.log('Cpu choose paper');   
+    }  else if (randomCpu == 3) {
+        console.log('Cpu choose scissors')
+    } 
+}
+ 
+cpu();
 
 
+// function user() {
+//     const prompt = require("prompt-sync")({ sigint: true});
+//     let sign = prompt("Choose Your weapon: rock paper or scrissors");
+// } if (sign === rock &&) {
+//     console.log
+// }
+
+
+
+
+
+
+
+
+ 
