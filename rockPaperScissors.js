ee7e144becd6878a88ea6f31df3ea53f39d36d85
Diego Medina <damgz191@gmail.com>
2022-08-16 22:10:05 -0500

    "commit with the 3 functions but the third one still doesn't work"

--- rockPaperScissors.js
+++ rockPaperScissors.js
@@ -4,27 +4,53 @@ rock = 1;
 paper = 2;
 scissors = 3;
 
+function user() {
+    const prompt = require("prompt-sync")({ sigint: true});
+    let sign = prompt("Choose Your weapon: rock paper or scissors?: ");
+}
+
 let randomCpu = Math.floor(Math.random() * 3) + 1;
 
 function cpu() {
     if (randomCpu == 1) {
-        console.log('Cpu Choose rock');   
+        console.log('rock');   
     }  else if (randomCpu == 2) {
-        console.log('Cpu choose paper');   
+        console.log('paper');   
     }  else if (randomCpu == 3) {
-        console.log('Cpu choose scissors')
+        console.log('scissors')
     } 
 }
- 
+
+function result(user, cpu) {
+    if(user == rock && cpu == paper) {
+       return console.log('cpu wins!');
+    } else if (user == rock && cpu == scissors) {
+        return console.log('user wins!');
+    } else if (user == rock && cpu == rock) {
+        return console.log("It\'s a Draw, Play Again! ");
+    } else if (user == paper && cpu == rock) {
+        return console.log('user wins!');
+    } else if (user == paper && cpu == scissors) {    
+        return console.log('cpu wins!');
+    } else if (user == paper && cpu == paper) {    
+        return console.log("It\'s a Draw, Play Again! ");
+    } else if (user == scissors && cpu == rock) {
+        return console.log('cpu wins!');
+    } else if (user == scissors && cpu == paper) {    
+        return console.log('user wins!');
+    } else if (user == scissors && cpu == scissors) {    
+        return console.log("It\'s a Draw, Play Again! ");
+    } 
+}
+
+
+
+user();
 cpu();
+result(paper);
 
 
-// function user() {
-//     const prompt = require("prompt-sync")({ sigint: true});
-//     let sign = prompt("Choose Your weapon: rock paper or scrissors");
-// } if (sign === rock &&) {
-//     console.log
-// }
+
 
 
 
