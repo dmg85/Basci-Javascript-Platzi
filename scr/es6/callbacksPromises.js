ce1791c401ade3076eb520b4f1ed165ee893ae63
Diego Medina <damgz191@gmail.com>
2022-10-16 18:14:19 -0500

    regex example

--- scr/es6/callbacksPromises.js
+++ scr/es6/callbacksPromises.js
@@ -21,4 +21,12 @@ helloWorld()
     .finally(() => console.log('Finalized'))
     
     
+const regexData = /([0-9]{4})-([0-9]{2})-([0-9]{2})/
+const match = regexData.exec('2020-10-16');
+const year = match[1]
+const month = match[2]
+const day = match[3]
+
+console.log(year, month, day);
+
 
